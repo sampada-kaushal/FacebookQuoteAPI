63497a0d9c1e00dbaf01b4757091e5d0c2029faa
sampada-kaushal <sampada_kaushal@rediffmail.com>
2017-06-15 13:55:32 +0530

    Create Runner.java

---
+++ QuoteGenRunner/src/Runner.java
@@ -0,0 +1,16 @@
+import com.fetch.*;
+
+
+public class Runner {
+public static void main(String[] args)
+{
+	 try {
+		Facebook r=new Facebook();
+	} 
+	 catch (Exception e) {
+				e.printStackTrace();
+	}
+	
+	System.out.println("Image post successfully done! Check your Facebook account");
+}
+}
